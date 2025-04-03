47b4ac70b6a438f0e6cc658ddb9b0cd83dc643bd
Rey Hall <reyh4ll@gmail.com>
2025-04-03 09:57:15 -0300

    NS-15 [Backend] C# + Unity | Cenario color random

--- Assets/Scripts/Background/MovimentBackground.cs
+++ Assets/Scripts/Background/MovimentBackground.cs
@@ -1,28 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MovimentBackground : MonoBehaviour
-{ 
+{
     public float velocity = 0.1f;
     public bool moveHorizontal = false;
     public bool invertY = false;
-
+    public ColorChanger colorChanger;
     private Renderer render;
     private Vector2 offset;
 
-    void Start () 
+    private void Start()
     {
         render = GetComponent<Renderer>();
+        if (colorChanger != null) colorChanger.Initialize(render);
     }
 
-    void Update()
+    private void Update()
     {
-        if (render != null){
+        if (render != null)
+        {
             offset.x += moveHorizontal ? velocity * Time.deltaTime : 0;
             offset.y += velocity * Time.deltaTime * (invertY ? -1 : 1);
-
             render.material.mainTextureOffset = offset;
+            
+            if (colorChanger != null) colorChanger.UpdateColor();
         }
     }
 }
