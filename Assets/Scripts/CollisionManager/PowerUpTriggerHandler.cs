b2e0a6fd568807b2b76166cb7799c7e5874a6a6e
Rey Hall <reyh4ll@gmail.com>
2025-04-14 09:33:07 -0300

    NS-16 [Backend] C# + Unity | Adicionar Som de Colisão

--- Assets/Scripts/CollisionManager/PowerUpTriggerHandler.cs
+++ Assets/Scripts/CollisionManager/PowerUpTriggerHandler.cs
@@ -2,16 +2,26 @@ using UnityEngine;
 
 public class PowerUpTriggerHandler : MonoBehaviour
 {
-    private PowerUp powerUp;
+    public PowerUp powerUp;
 
     private void Awake()
     {
-        powerUp = GetComponent<PowerUp>();
+        if (powerUp == null)
+        {
+            powerUp = GetComponent<PowerUp>();
+            if (powerUp == null)
+            {
+                Debug.LogError("PowerUp component not found on the GameObject.");
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")) Destroy(gameObject);
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
         else if (collision.CompareTag("Shot"))
         {
             Destroy(collision.gameObject);
